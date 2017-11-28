0eedbf86afd7a8ceff16cd978a10f0560cd9a0e1
Alexey <reflectored@gmail.com>
2017-11-28 22:27:55 +0100

    Naming Convention

--- src/se/lib/Books.java
+++ src/se/lib/Books.java
@@ -11,7 +11,8 @@ class Books {
         return bookWeared;
     }
 
-    String getBookList() {
+    @Override
+    String toString() {
         StringBuilder booksInLibrary = new StringBuilder("");
             for (Book b : bookList) {
                 booksInLibrary.append(b.getBookDetails().toString());
